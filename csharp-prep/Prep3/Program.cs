3a9d745e28ecc69cf33fa634cc30d3b78995cc36
carrildan <97158875+carrildan@users.noreply.github.com>
2023-01-11 11:32:06 +0100

    created and changed the logic. Works fine

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -6,53 +6,61 @@ class Program
     {
         //generate a random number
         Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 100);
+        int magicNumber = randomGenerator.Next(1, 50);
+
 
         //keep track of user attempts
         int userAttempts = 0;
+
 
         //first question to explain there is a random number
         Console.WriteLine("What is the magic number? ");
         //string magicNumber = Console.ReadLine();
         //int intMagicNumber = int.Parse(magicNumber);
-        
-        //user guess
-        Console.Write("What is your guess? ");
-        string userInput = Console.ReadLine();
-        int intUserInput = int.Parse(userInput);
-        userAttempts += 1;
+    
 
         //while playAgain is = yes, it will play again
         string playAgain = "yes";
         while (playAgain == "yes")
         {
-            while (intUserInput != magicNumber)
-            {
-                if (intUserInput < magicNumber)
-                {
-                    Console.WriteLine("Higher");
-                    Console.Write("What is your guess? ");
-                    userAttempts += 1;
-                    userInput = Console.ReadLine();
-                    intUserInput = int.Parse(userInput);
-                }
-                else if (intUserInput > magicNumber)
-                {
-                    Console.WriteLine("Lower");
-                    Console.Write("What is your guess? ");
-                    userAttempts += 1;
-                    userInput = Console.ReadLine();
-                    intUserInput = int.Parse(userInput);
-                }                    
-            }
-            Console.WriteLine("You guessed it!");
-            Console.WriteLine($"Your total attemps is: {userAttempts}");
-            //asks the user if wants to play again.
-            Console.Write("Play Again? ");
-            playAgain = Console.ReadLine();            
-        }
-        //if user says no, game over
-        Console.Write("Game Over");
+            //user guess
+            Console.Write("What is your guess? ");
+            string userInput = Console.ReadLine();
+            int intUserInput = int.Parse(userInput);
+            userAttempts += 1;
 
+
+            if (intUserInput < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (intUserInput > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (intUserInput == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"Your total attempts is: {userAttempts}");
+                //erase the attempts when the user guesses correctly
+                userAttempts = 0;
+
+
+                //prompt the user to play again.
+                Console.Write("Play again? ");
+                playAgain = Console.ReadLine();         
+
+
+                //generates a new random number if user wants to play again
+                if (playAgain == "yes")
+                {
+                    magicNumber = randomGenerator.Next(1, 5);
+                    Console.WriteLine("What is the magic number? ");
+                }      
+            }
+        }
+        Console.Write("Game Over!");
+     
+  
     }
 }
