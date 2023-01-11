9c7f0b6ad7dc7ccd937a9a770b4870368b29dc36
carrildan <97158875+carrildan@users.noreply.github.com>
2023-01-11 13:56:23 +0100

    completed prep5 assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome()
+        {
+            Console.Write("Welcome to the Program!\n");
+        }
+        
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+        
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int userNumber = int.Parse(Console.ReadLine());
+            return userNumber;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int num = number;
+            return (num * num);
+        }
+
+        static void DisplayResult(string name, int square)
+        {
+            Console.Write($"{name}, the square of your favorite number is: {square}");
+
+        }
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int square = SquareNumber(number);
+        DisplayResult(name, square);
     }
 }
