22f576e2c5c8a33f885041cb14672667b0dbc47a
carrildan <97158875+carrildan@users.noreply.github.com>
2023-01-18 02:49:19 +0100

    created these files and their objects using abstr.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Accenture";
+        //job1.Display();        
+
+        Job job2 = new Job();
+        job2._jobTitle = "Web Developer";
+        job2._company = "Ocean";
+        //job2.Display();
+
+        Resume resume1 = new Resume();
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1._name = "Allison Rose";
+
+        resume1.Display();
     }
 }
---
+++ prepare/Learning02/job.cs
@@ -0,0 +1,16 @@
+public class Job
+{
+    public string _company = "";
+    public string _jobTitle = "";
+    public int _startYear = 2019;
+    public int _endYear = 2022;
+
+
+     public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}), {_startYear} - {_endYear}");
+    }
+}
+
+
+   
