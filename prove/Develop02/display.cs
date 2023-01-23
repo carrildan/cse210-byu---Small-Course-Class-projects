93edc8070c605a7b57ffa81e832ed23cb088316a
carrildan <97158875+carrildan@users.noreply.github.com>
2023-01-23 14:50:02 +0100

    nothing

---
+++ prove/Develop02/display.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Display
+{
+    //public Journal _printQuestions = new Journal();
+
+
+    public void displayPrompt()
+    {
+       // Console.WriteLine($"{_printQuestions}");
+    }
+}
