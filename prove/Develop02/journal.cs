426216cb3e3c370cf79bc02c149b69d7cea87fc4
carrildan <97158875+carrildan@users.noreply.github.com>
2023-01-23 10:52:12 +0100

    created journal class, a prompt(). Added questions

---
+++ prove/Develop02/journal.cs
@@ -0,0 +1,29 @@
+using System;
+
+public class Journal
+{
+    //member variables. list for questions creation and user entries prompt
+    public List<string>_questions = new List<string>(); 
+    public List<string>_entry = new List<string>();
+    
+
+    //method to create the questions
+    public void Prompt()
+    {
+        //adding a list of questions for the journal
+        string question1 = "Describe a spiritual experience you had today:";
+        string question2 = "Did you meet a different or special person today?";
+        string question3 = "Did you meet a different or special place today?";
+        _questions.Add(question1);
+        _questions.Add(question2);
+        _questions.Add(question3);
+
+
+        foreach (string q in _questions)
+        {
+            Console.WriteLine(q);
+        }
+
+    }
+    
+}
