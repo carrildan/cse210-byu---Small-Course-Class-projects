01fb79cc693de12ef1a57c9b9f8db5dfe60dc802
carrildan <97158875+carrildan@users.noreply.github.com>
2023-01-23 10:08:32 +0100

    welcome message, menu, user input

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        //render a welcome message
+        Console.WriteLine("Welcome to the Journal Program!");
+        Console.WriteLine("Please select one of the following choices:");
+
+        
+        //menu options
+        string menu = ("1. Write \n2. Display \n3. Load \n4. Save\n5. Quit");
+        Console.WriteLine(menu);
+
+        //user entry the option
+        Console.Write("What would you like to do? ");
+        string user = Console.ReadLine();
+        int userInt = int.Parse(user);
     }
 }
