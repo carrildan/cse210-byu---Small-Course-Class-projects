bd4040da6d7583ce87a611d62034cf5217847e31
carrildan <97158875+carrildan@users.noreply.github.com>
2023-01-27 18:11:38 +0100

    in Save() I added a paramater that receives the attributs from Program class option 4

--- prove/Develop02/file.cs
+++ prove/Develop02/file.cs
@@ -2,30 +2,33 @@ using System.IO;
 using System.Collections.Generic;
 
 public class File
-{
-    public Journal _journalFile = new Journal();
-    public List<string> _file = new List<string>();
+{   //FEEDBACK - USING PARAMETERS 
+    //public Journal _journalFile = new Journal();
+    //public List<Journal> _file = new List<Journal>();
+    public string _fileName;
 
 
     //method to save a new Journal entry
     /*method that saves the user´s entries as a file
     prompting for the name of the file
     It is called by option: Save in the menu*/
-    public void Save()
+    public void Save(List<string>entries)
     {
         
-        //var stringList = String.Join(",", Display().ToArray());
         //Console.Write(stringList);
         Console.Write("Give a name to your file: ");
-        string fileName = Console.ReadLine();
+        _fileName = Console.ReadLine();
 
         
-        using (StreamWriter outputFile = new StreamWriter(fileName))
+        using (StreamWriter outputFile = new StreamWriter(_fileName))
         {
+            
             //convert the list into string
             //I found this approach in YouTube
-            var stringList = String.Join("\n", _entry.ToArray());//I NEED TO GET ACCESS TO THE _entry variable in Journal
-            outputFile.WriteLine( stringList);
+            //var stringList = String.Join("\n", _file.ToArray());
+            //var stringList = _file;
+            foreach (var item in entries)
+                outputFile.WriteLine(item);
         }
     }
 
@@ -34,7 +37,9 @@ public class File
     //it´s called by option: Load in the menu
     public void Load()
     {
-        string fName = "teste.txt";//I HAVE TO CHANGE TO THE FILE THE USER ENTRY
+        Console.Write("Open a file: ");
+        _fileName = Console.ReadLine();
+        string fName = _fileName;//I HAVE TO CHANGE TO THE FILE THE USER ENTRY
         string[] lines = System.IO.File.ReadAllLines(fName);
 
 
