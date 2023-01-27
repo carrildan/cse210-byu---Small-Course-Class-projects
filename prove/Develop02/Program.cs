485f9bf44c6faccd21445ed1de60605cc48658bf
carrildan <97158875+carrildan@users.noreply.github.com>
2023-01-27 18:08:05 +0100

    created a journal and a file at the top to call their methods inside the conditional | Option 4(save) added a new entry as a parameter in Save(journal1._entry) | Option 3 (load) called load()

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
@@ -8,17 +9,61 @@ class Program
         Console.WriteLine("Welcome to the Journal Program!");
         Console.WriteLine("Please select one of the following choices:");
 
-        
-        //menu options
-        string menu = ("1. Write \n2. Display \n3. Load \n4. Save\n5. Quit");
-        Console.WriteLine(menu);
 
-        //user entry the option
-        Console.Write("What would you like to do? ");
-        string user = Console.ReadLine();
-        int userInt = int.Parse(user);
+        //creates a new journal (because if creates inside a conditional block I won´t have access in another block of the conditional)
+        Journal journal1 = new Journal();
+        File file1 = new File();
 
-       
-        
+
+
+        //mantain prompting user for a option until chooses to exit
+        int userInt = -1;
+        while (userInt != 5)
+        {
+            //menu options
+            string menu = ("1. Write \n2. Display \n3. Load \n4. Save\n5. Quit");
+            Console.WriteLine(menu);
+
+            //user entry the option
+            Console.Write("What would you like to do? ");
+            string user = Console.ReadLine();
+            userInt = int.Parse(user);
+
+
+            /* ---------------  menu conditional ----------------- */
+            
+            
+            //Option 1 - Write a new journal or update a journal
+            if (userInt == 1)
+
+            {
+                journal1.Prompt();
+                journal1.Entries();
+            }
+
+
+            //Option 2 - Display the current Write journal
+            else if (userInt == 2)
+            {
+                //displays the current entries
+                journal1.Display();
+            }
+
+
+            //Option 4 - Saves to a txt file
+            else if (userInt == 4)
+            {
+                file1.Save(journal1._entry);
+            }
+
+
+            //Option 3 - Load a file
+            else if (userInt == 3)
+            {
+                file1 = new File();
+                file1.Load();
+            }
+            
+        }   
     }
 }
