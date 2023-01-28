14145c2ddf4f7798ff80eec5b17a4600d87e5555
carrildan <97158875+carrildan@users.noreply.github.com>
2023-01-28 07:49:56 +0100

    created display() to display a file content | a list holds the content of the file so it can be accessed by display()

--- prove/Develop02/file.cs
+++ prove/Develop02/file.cs
@@ -6,12 +6,13 @@ public class File
     //public Journal _journalFile = new Journal();
     //public List<Journal> _file = new List<Journal>();
     public string _fileName;
+    public List<string> _newFile = new List<string>();
+
 
 
     //method to save a new Journal entry
-    /*method that saves the user´s entries as a file
-    prompting for the name of the file
-    It is called by option: Save in the menu*/
+    //method that saves the user´s entries as a file prompting for the name of the file. It is called by option: Save in the menu//
+    //Save() parameter receives the attributes from Program class in option 4
     public void Save(List<string>entries)
     {
         
@@ -41,11 +42,21 @@ public class File
         _fileName = Console.ReadLine();
         string fName = _fileName;//I HAVE TO CHANGE TO THE FILE THE USER ENTRY
         string[] lines = System.IO.File.ReadAllLines(fName);
-
-
+        
         foreach(string line in lines)
         {
-            Console.WriteLine(line);//I HAVE TO CHANGE SO IT DOES NOT PRINT ON CONSOLE. SHOULD ONLY OPEN A FILE
+            _newFile.Add(line);
+        }
+        
+
+    }
+
+    //method that displays the content in the file
+    public void Display()
+    {
+        foreach(string line in _newFile)
+        {
+            Console.WriteLine(line);
         }
     }
 }
