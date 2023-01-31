927f2338b0d55d0f9bbcbd80ae9386d84d8588aa
carrildan <97158875+carrildan@users.noreply.github.com>
2023-01-31 20:17:42 +0100

    called and consoled methods

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction(1, 1);
+        Fraction fraction2 = new Fraction(6, 1);
+        Fraction fraction3 = new Fraction(6, 7);
+
+        fraction1.SetTop(1);
+
+        fraction1.SetBottom(3);
+        
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.Write(fraction1.GetDecimalValue());
+        
     }
 }
