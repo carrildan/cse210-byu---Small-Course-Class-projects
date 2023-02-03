c8de4a7e26bf3e80f6a65348f45f175835cab5c8
carrildan <97158875+carrildan@users.noreply.github.com>
2023-02-03 15:43:22 +0100

    created word class and added method and attributes

---
+++ prove/Develop03/word.cs
@@ -0,0 +1,41 @@
+using System;
+
+//this class is responsible for hidden words and display the scripture
+class Word
+{
+    string _scripture;
+    string _hiddenWords;
+
+
+    //constructor
+    public Word()
+    {
+
+    }
+
+
+    //method to hidden the words 
+    public void ToHidden()
+    {
+
+    }
+
+
+    //method to display a single verse scripture
+    public void Display()
+    {
+
+    }
+
+
+    //method to display a multiple verse scripture
+    public void DisplayMultipleVerse()
+    {
+        
+    }
+
+
+
+
+
+}
