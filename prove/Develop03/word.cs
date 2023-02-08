010bc13ca56337632380adc5c4a051ceeff5c3e9
carrildan <97158875+carrildan@users.noreply.github.com>
2023-02-08 18:11:00 +0100

    replaced specific word into ---

--- prove/Develop03/word.cs
+++ prove/Develop03/word.cs
@@ -3,20 +3,55 @@ using System;
 //this class is responsible for hidden words and display the scripture
 class Word
 {
-    string _scripture;
-    string _hiddenWords;
+    private Scriptures _scripture;
+    //private string _hiddenWords;
+    private List<string> _hiddenWords;
 
 
     //constructor
     public Word()
     {
-
+        _scripture = new Scriptures();
+        _hiddenWords = new List<string>();
+        
     }
 
 
     //method to hidden the words 
     public void ToHidden()
     {
+        //call random method
+        Random rnd = new Random();
+        string words = _scripture.GetMultipleVerses();
+        string getWords  = words.Replace(" the ", "---");
+        foreach(char a in getWords)
+        {
+            _hiddenWords.Add(a.ToString());
+        }
+        foreach(string i in _hiddenWords)
+        {
+            Console.Write(i);
+
+        }
+        //string[] splitWords = words.Split(" ");
+        
+
+        
+
+        
+        //int index = rnd.Next(splitWords.Length);
+        //string getWords  = splitWords[index].Replace("the", "---");
+       
+
+      
+
+        
+
+        //int index = rnd.Next(splitWords.Length);
+        //string getWords  = splitWords[index];
+        //Console.WriteLine(splitWords);
+        //string replacer = splitWords[index].Replace(getWords,"---");
+        //_hiddenWords = replacer;
 
     }
 
@@ -24,7 +59,8 @@ class Word
     //method to display a single verse scripture
     public void Display()
     {
-
+        
+        Console.Write(_hiddenWords);
     }
 
 
