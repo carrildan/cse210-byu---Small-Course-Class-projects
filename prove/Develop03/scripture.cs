c574826875d0e9ae4ce95092c20c251e872d7c60
carrildan <97158875+carrildan@users.noreply.github.com>
2023-02-15 00:32:59 +0100

    got random generator word to work

--- prove/Develop03/scripture.cs
+++ prove/Develop03/scripture.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 class Scripture
 {
@@ -6,7 +7,7 @@ class Scripture
    string _scripture;
 
    //attribute that stores and tracks the words of the scripture
-   string _words;
+   List<string> _words = new List<string>();
 
 
    //constructor for a single verse scripture
@@ -26,6 +27,31 @@ class Scripture
    //method to hide the word
    public void HideWords()
    {
+        //split the letters into words
+        string[] words = _scripture.Split(" ");
+
+        //adding each word of the scripture into to the list so it can hide it. I WASN´T ABLE TO HANDLE HIDDING PROCESS WITHOUT ADDING IT TO A LIST.
+        foreach(string item in words)
+        {
+         _words.Add(item);
+        }
+
+
+        //generates a random word inside the list so it hides randomly
+        Random rnd = new Random();
+        string rndWord = _words[rnd.Next(0,_words.Count())];
+        Console.Write(rndWord);
+
+
+        //hides the targeted word 
+        for(int i=0;i<_words.Count;i++)
+        {
+            if(_words[i].Contains("Nephi"))
+                _words[i] = "---";
+        }  
+
+        
+
         
    }
 }
