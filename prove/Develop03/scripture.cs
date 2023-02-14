06c7bbfc26b18000861dbdef206fd0de91179ca6
carrildan <97158875+carrildan@users.noreply.github.com>
2023-02-14 14:21:24 +0100

    created two cunstructors. Both works fine

--- prove/Develop03/scripture.cs
+++ prove/Develop03/scripture.cs
@@ -1,9 +1,33 @@
 using System;
 
-class Scriptures
+class Scripture
 {
-   
+   //attribute that stores a scripture
+   string _scripture;
+
+   //attribute that stores and tracks the words of the scripture
+   string _words;
 
 
+   //constructor for a single verse scripture
+   public Scripture(string scripture)
+   {
+        _scripture = scripture;
+   }
 
+
+    //constructor for multiple verses scripture
+   public Scripture(string verse, string multipleVerses )
+   {
+    _scripture = verse + multipleVerses;
+   }
+
+
+   //method to hide the word
+   public void HideWords()
+   {
+        
+   }
 }
+
+
