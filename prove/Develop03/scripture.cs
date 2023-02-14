9dc7a27ca826a28cf880569c3b9314e9e5d4ab4b
carrildan <97158875+carrildan@users.noreply.github.com>
2023-02-14 13:23:40 +0100

    restarting the program design and code from zero

--- prove/Develop03/scripture.cs
+++ prove/Develop03/scripture.cs
@@ -2,65 +2,7 @@ using System;
 
 class Scriptures
 {
-    private string _scripture;
-    private string _multiScripture;
-    private string _reference;
-    private string _multipleReference;
-
-
-    //constructor for single verse scripture
-    public Scriptures()
-    {
-        _scripture = "7. And it came to pass that I, Nephi, said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them.";
-
-        _multiScripture = "";
-
-        _reference = "1 Nephy 3:7";
-
-        _multipleReference = "";
-    }
-
-
-    //constructor for multiple reference
-    public Scriptures(string multiReference)
-    {
-         _scripture = "7. And it came to pass that I, Nephi, said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them.";
-
-        _multiScripture = "8 And it came to pass that when my father had heard these words he was exceedingly glad, for he knew that I had been blessed of the Lord.";
-
-        _reference = "1 Nephy 3:7";
-
-        _multipleReference = "1 Nephy 3:7-8";
-    }
-
-
-    //getter and setter for single reference
-    public string GetScriptures()
-    {
-        return _scripture;
-    }
-
-
-    public void SetScriptures()
-    {
-        //Word word = new Word();
-
-        //unused
-    }
-
-
-    //getter and setter for multiple verse scripture
-    public string GetMultipleVerses()
-    {
-        return $"{_multipleReference} - {_scripture} {_multiScripture}";
-    }
-
-
-    public void SetMultipleVerses()
-    {
-        //unused
-    }
-    
+   
 
 
 
--- prove/Develop03/word.cs
+++ prove/Develop03/word.cs
@@ -4,63 +4,9 @@ using System.Collections.Generic;
 //this class is responsible for hidden words and display the scripture
 class Word
 { //TO DO: CHANGING THE _SCRITPURE ATTRIBUTE TO RECEIVE A STRING, THEN CREATE A NEW LIST TO STORE THE WORDS.
-    private string _scripture;
+    private Scriptures _scripture;
     //private string _hiddenWords;
-    private List<string> _hiddenWords;
-
-
-    //constructor
-    public Word()
-    {
-        List<string> _scripture = new List<string>();
-        _hiddenWords = new List<string>();
-        
-    }
-
-
-    //method to hidden the words 
-    public void ToHidden()
-    {
-        //call random method
-        Random rnd = new Random();
-
-        //get scripture and split into words
-        string scripture = _scripture.GetScriptures();
-        string[] splitWords = scripture.Split("");
-
-        //add each word to the list
-        foreach(var item in splitWords)
-        {
-            _hiddenWords.Add(item);
-        }
-
-        
-        foreach(string item in _hiddenWords)
-        {
-            Console.Write(item);
-        }
-
-    }
-
-
-
-
-
-    //method to display a single verse scripture
-    public void Display()
-    {
-        
-        //Console.Write(_hiddenWords);
-    }
-
-
-    //method to display a multiple verse scripture
-    public void DisplayMultipleVerse()
-    {
-        
-    }
-
-
+    
 
 
 
