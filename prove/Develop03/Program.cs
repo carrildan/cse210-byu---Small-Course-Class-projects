d61f53c2ffcc01d064fc0e50cf8178869ce26ec6
carrildan <97158875+carrildan@users.noreply.github.com>
2023-02-15 01:53:57 +0100

    added a scripture to the word class

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,15 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        //calls scripture class and displays the scripture
-        Word scripture = new Word();
-        scripture.ToHidden();
-        //scripture.Display();
+        string user = "";
+        while (user != "quit")
+       { 
+
+        //creates a new scripture
+        Scripture newScripture = new Scripture("7 And it came to pass that I, Nephi, said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them.");
+        //newScripture.HideWords();
+
+        Word newWord = new Word(newScripture.HideWords());
+        newWord.IsHidden();
 
         //prompts the question
         Console.WriteLine("\nPlease enter to continue or type ´quit` to finish:");
-        string user = Console.ReadLine();
-
+        user = Console.ReadLine();
+       }
         //Console.Clear();
         //Console.WriteLine("BBB");
     }
