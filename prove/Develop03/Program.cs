0a62afbf5ba3dc8c835e892579d8e6a73e6ea29d
carrildan <97158875+carrildan@users.noreply.github.com>
2023-02-15 18:10:03 +0100

    print a full scripture at the start of the program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,22 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
+        //creates a new scripture
+        Scripture newScripture = new Scripture("7 And it came to pass that I, Nephi, said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them.");
+        
+
+        //render a scripture
+        Console.WriteLine(newScripture.getScripture());
+        Console.WriteLine("");
+
+
         string user = "";
         while (user != "quit")
        { 
 
-        //creates a new scripture
-        Scripture newScripture = new Scripture("7 And it came to pass that I, Nephi, said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them.");
-        //newScripture.HideWords();
+        
 
         Word newWord = new Word(newScripture.HideWords());
         newWord.IsHidden();
+        newWord.Display();
 
         //prompts the question
         Console.WriteLine("\nPlease enter to continue or type ´quit` to finish:");
         user = Console.ReadLine();
+
+        Console.Clear();
+        
        }
-        //Console.Clear();
+        
         //Console.WriteLine("BBB");
     }
 }
