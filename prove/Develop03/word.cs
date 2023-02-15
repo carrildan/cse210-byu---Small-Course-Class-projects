ef6b7b6cd01d3838e9e65562bf2d1ccefc848901
carrildan <97158875+carrildan@users.noreply.github.com>
2023-02-15 01:50:10 +0100

    added attributes, constructor and now is receiving the hidden words from Scripture class

--- prove/Develop03/word.cs
+++ prove/Develop03/word.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 
 //this class is responsible for hidden words and display the scripture
 class Word
-{ //TO DO: CHANGING THE _SCRITPURE ATTRIBUTE TO RECEIVE A STRING, THEN CREATE A NEW LIST TO STORE THE WORDS.
-    private Scriptures _scripture;
-    //private string _hiddenWords;
+{ 
+    //receives a word from Scripture
+    string[] _word;
+
+    //stores and tracks the words that are hidden.
+    List<string> _hiddenWords = new List<string>();
+
+
+    //sets the attributes 
+    public Word(string[] words)
+    {
+        //receives the hidden words from Scripture class
+        _word = words;
+    }
+
+
+    //returns true or false for a hidden word
+    public void IsHidden()
+    {
+        //adds the hidden words to the list
+        foreach(string item in _word)
+        {
+            _hiddenWords.Add(item);
+
+        }
     
-
-
-
+        foreach(string item in _hiddenWords)
+        {
+            Console.Write(item);//TEST
+        }
+    }
 }
