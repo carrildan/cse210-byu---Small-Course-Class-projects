f4930f83d2d45a523881a8dc25632aa154a9f225
carrildan <97158875+carrildan@users.noreply.github.com>
2023-02-20 12:09:52 +0100

    copy of changes from test2 branch | also changed while loop and called addingWords() outside the loop

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
+        //creates a new scripture
+        Scripture newScripture = new Scripture("A wise man will hear, and will increase learning; and a man of understanding shall attain unto wise counsels:");
+        newScripture.addingWords();
+
+        //render a scripture
+        Console.WriteLine(newScripture.getScripture());
+        Console.WriteLine("");
+
+
+        Console.WriteLine("Choose your option");
+        string user = Console.ReadLine();
+
+        while (user != "quit")
+       { 
+        newScripture.HideWords();
+        newScripture.Display();
+        Console.WriteLine("Choose your option");
+        user = Console.ReadLine();
+        //prompts the question
         
+        
+       }
+        
+        //Console.WriteLine("BBB");
     }
 }
