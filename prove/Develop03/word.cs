2de57cdf0404792c22f59ec04b3709e08d9e3916
carrildan <97158875+carrildan@users.noreply.github.com>
2023-02-20 12:05:18 +0100

    copy of chanages from test2 branch

--- prove/Develop03/word.cs
+++ prove/Develop03/word.cs
@@ -3,104 +3,39 @@ using System.Collections.Generic;
 
 //this class is responsible for hidden words and display the scripture
 class Word
-{
-    private Scriptures _scripture;
-    //private string _hiddenWords;
-    private List<string> _hiddenWords;
+{ 
+    //receives a word from Scripture
+    string[] _word;
+
+    //stores and tracks the words that are hidden.
+    List<string> _hiddenWords = new List<string>();
 
 
-    //constructor
-    public Word()
+    //sets the attributes 
+    public Word(string[] words)
     {
-        _scripture = new Scriptures();
-        _hiddenWords = new List<string>();
-        
+        //receives the hidden words from Scripture class
+        _word = words;
     }
 
 
-    //method to hidden the words 
-    public void ToHidden()
+    //returns true or false for a hidden word
+    public void IsHidden()
     {
-        //call random method
-        Random rnd = new Random();
-
-        //get the scripture
-        string words = _scripture.GetScriptures();
-        string splitWords = string.Join(" ",words);
-
-        foreach (var item in words)
+        //adds the hidden words to the list
+        foreach(string item in _word)
         {
-            _hiddenWords.Add(item.ToString());
-            
+            _hiddenWords.Add(item);
+
         }
-        foreach(string item in _hiddenWords)
-        {
-            Console.Write(item);
-        };
-        //split it into words then separete with space
-        
-        //Console.Write(words.GetType());
-        //string unsplitWords = string.Join("--",words);
-        //Console.Write(unsplitWords);
-
-        //string listWords = unsplitWords;
-        //Console.Write(listWords.GetType());
-
-    /*
-        for( i in listWords){
-            _hiddenWords.Add(i);
-            Console.Write(i);
-        }
-
-
-
-        for(int i=0;i<_hiddenWords.Count;i++)
-        {
-            if(_hiddenWords[i].Contains("Nephi"))
-                _hiddenWords[i] = "Daniel";
-                Console.Write(_hiddenWords[i]);
-        }   
-
-        foreach(string item in _hiddenWords)
-        {
-            Console.WriteLine(item);
-        }  
-
-    */
-
-
-        //string[] splitWords = words.Split(" ");
-        
-
-        //int index = rnd.Next(splitWords.Length);
-        //string getWords  = splitWords[index].Replace("the", "---");
-       
-
-        //int index = rnd.Next(splitWords.Length);
-        //string getWords  = splitWords[index];
-        //Console.WriteLine(splitWords);
-        //string replacer = splitWords[index].Replace(getWords,"---");
-        //_hiddenWords = replacer;
-
+    
     }
-
-
-    //method to display a single verse scripture
     public void Display()
     {
-        
-        //Console.Write(_hiddenWords);
+        foreach(string word in _hiddenWords)
+        {
+        Console.Write($"{word}");
+        }
     }
-
-
-    //method to display a multiple verse scripture
-    public void DisplayMultipleVerse()
-    {
         
-    }
-
-
-
-
-
 }
