75657df35840d23008550d793dc6bb01effc0a96
carrildan <97158875+carrildan@users.noreply.github.com>
2023-02-21 12:26:07 +0100

    added method and attributes

--- prove/Develop03/reference.cs
+++ prove/Develop03/reference.cs
@@ -2,6 +2,36 @@ using System;
 
 class Reference
 {
-    //attribute that stores a scripture
-    string _scripture;
+    //stores a book
+    string _book;
+    //stores a chapter
+    string _chapter;
+    //stores the first verse
+    string _firtVerse;
+    //stores the rest of the verses 
+    string _lastVerse;
+
+
+    public Reference(string book, string chapter, string firstVerse, string lastVerses)
+    {
+        _book = book;
+        _chapter = chapter;
+        _firtVerse = firstVerse;
+        _lastVerse = lastVerses;
+    }
+
+
+    public string GetReference()
+    {
+        return $"{_book} {_chapter}: {_firtVerse} - {_lastVerse}";
+    }
+
+
+    public void SetReference()
+    {
+        
+    }
+
 }
+
+
