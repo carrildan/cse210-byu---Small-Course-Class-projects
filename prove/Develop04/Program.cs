18e4b67fa36b2933963cbe8461a187dd4e868444
carrildan <97158875+carrildan@users.noreply.github.com>
2023-02-26 22:25:35 +0100

    created a new Activity to test Activity class

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Activity activity = new Activity();
+        activity.DisplayStartMessage();
+        activity.DisplayEndMessage();
     }
 }
