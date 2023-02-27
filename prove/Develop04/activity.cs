c2e9ed61262412738817cf05bd66ab108563d887
carrildan <97158875+carrildan@users.noreply.github.com>
2023-02-27 10:32:36 +0100

    added the activity description text and activity duration text | created the PausingSpinner()

--- prove/Develop04/activity.cs
+++ prove/Develop04/activity.cs
@@ -23,7 +23,14 @@ class Activity
     //displays the start message
     public void DisplayStartMessage()
     {
-        Console.WriteLine($"Welcome to the {_activityName} activity.");
+        //welcome message and description of the activity
+        Console.WriteLine($"Welcome to the {_activityName} activity.\n");
+        Console.WriteLine($"{_activityDescription}\n");
+
+
+        //prompts the user for the time duration of the activity
+        Console.Write("How long, in seconds, would you like for your session? ");
+        _activityDuration = int.Parse(Console.ReadLine());
     }
 
 
@@ -33,4 +40,42 @@ class Activity
         Console.WriteLine("Well done!!");
         Console.WriteLine($"You have completed another {_activityDuration} seconds of the {_activityName} activity");
     }
+
+
+    //creates a spinner to load while program is paused
+    public void PausingSpinner()
+    {
+        //forms a spinner
+        List<string> animationStrings = new List<string>();
+        animationStrings.Add("|");
+        animationStrings.Add("/");
+        animationStrings.Add("-");
+        animationStrings.Add("\\");
+        animationStrings.Add("|");
+        animationStrings.Add("/");
+        animationStrings.Add("-");
+        animationStrings.Add("\\");
+
+
+        //message that appears above the spinner for all activities
+        Console.WriteLine("Get ready...");
+
+
+        //display spinner animation
+        foreach(string symbol in animationStrings)
+        {
+            Console.Write(symbol);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
+    }
+
+
+    public void PausingCountdown()
+    {
+        
+    }
+
+
+
 }
