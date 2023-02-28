70845e492e36c134c41438469a90ec6b177856ed
carrildan <97158875+carrildan@users.noreply.github.com>
2023-02-28 22:42:33 +0100

    creating breathingprogram

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,9 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        //creats Breathing Activity
         BreathingActivity breathingActivity = new BreathingActivity("Breath in...", "Now breath out...", "Breathing", "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing", 5);
         breathingActivity.DisplayStartMessage();
         breathingActivity.RunBreathingActivity();
-        
+        breathingActivity.DisplayEndMessage();
     }
 }
