d986d12fe0bd554e82d44dd5fc32e8d892f86768
carrildan <97158875+carrildan@users.noreply.github.com>
2023-02-28 23:37:49 +0100

    added a new attribute called _prompt to hold a single question | created SetRandomPrompt()

--- prove/Develop04/reflectingactivity.cs
+++ prove/Develop04/reflectingactivity.cs
@@ -7,6 +7,8 @@ public class ReflectingActivity : Activity
     private List<string> _listOfReflectingPrompts = new List<string>(){"Think of a time when you stood up for someone else.", "Think of a time when you did something really difficult.", "Think of a time when you helped someone in need.", "Think of a time when you did something truly selfless."};
     private List<string> _listOfReflectingQofPrompts = new List<string>();
 
+    private string _prompt;
+
 
     public ReflectingActivity(string name, string description, int duration) : base(name, description, duration)
     {
@@ -14,14 +16,20 @@ public class ReflectingActivity : Activity
     }
 
 
-    //get random prompts
+    //returns a prompt
     public string GetRandomPrompt()
+    {
+        return ($"{_prompt}");
+    }
+
+
+    //get random prompt and set it as attribute
+    public void SetRandomPrompt()
     {
         Random rnd = new Random();
         int rndIndex = rnd.Next(0,_listOfReflectingPrompts.Count); 
         string rndPrompt = _listOfReflectingPrompts[rndIndex];
 
-        return rndPrompt;
-
+        _prompt = rndPrompt;
     }
 }
