3314d640f36a32c3c89cf69b602949b846bffff4
carrildan <97158875+carrildan@users.noreply.github.com>
2023-02-28 14:42:39 +0100

    testing program

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,8 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        BreathingActivity breathingActivity = new BreathingActivity("breath in...", "breath out...", "Breathing", "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing", 5);
-        //breathingActivity.DisplayStartMessage();
-        //breathingActivity.DisplayEndMessage();
+        BreathingActivity breathingActivity = new BreathingActivity("Breath in...", "Now breath out...", "Breathing", "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing", 5);
+        breathingActivity.DisplayStartMessage();
+        breathingActivity.DisplayBreathMessage();
     }
 }
