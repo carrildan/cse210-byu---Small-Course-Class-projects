7aeca949c7df0d328de4f75af2286a77e510968c
carrildan <97158875+carrildan@users.noreply.github.com>
2023-03-02 11:46:48 +0100

    added _prompt to hold a single prompt | created a Getter and Setter

--- prove/Develop04/listingactivity.cs
+++ prove/Develop04/listingactivity.cs
@@ -7,10 +7,30 @@ public class ListingActivity : Activity
     private List<string> _listOfListingPrompts;
     //holds a list for all answers to the prompts
     private List<string> _listOfListingAnswerPrompts;    
+    //holds a single prompt
+    private string _prompt;
 
 
     public ListingActivity(string name, string description, int duration) : base(name, description, duration)
     {
         _listOfListingPrompts = new List<string>(){"Who are people that you appreciate?", "What are personal strengths of yours?", "Who are people that you have helped this week?", "When have you felt the Holy Ghost this month?", "Who are some of your personal heroes?"};
+    }
+
+
+    //return a single prompt
+    public string GetListingPrompt()
+    {
+        return $"{_prompt}";
+    }
+
+
+    //sets a single prompt
+    public void SetListingPrompt()
+    {
+        Random rnd = new Random();
+        int rndIndex = rnd.Next(0, _listOfListingPrompts.Count);
+        string rndWord = _listOfListingPrompts[rndIndex];
+
+        _prompt = rndWord;
     }
 }
