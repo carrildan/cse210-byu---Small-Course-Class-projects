f943996341da7a6236100453adde6d8fa165ac4b
carrildan <97158875+carrildan@users.noreply.github.com>
2023-03-07 19:40:19 +0100

    changed class to not abstract(because I do not need abstract by now) | added _totalPoints, _listOfGoals, _isCompleted attributes | created the virtual CreateGoal() | created GetListOfGoal()

--- prove/Develop05/goal.cs
+++ prove/Develop05/goal.cs
@@ -1,6 +1,6 @@
 using System;
 
-public abstract class Goal
+public class Goal
 {
     //name of the goal
     private string _goalName;
@@ -8,6 +8,12 @@ public abstract class Goal
     private string _goalDescription;
     //set points according to each goal
     private int _goalAssociatedPoints;
+    //stores the total points earned
+    private int _totalPoints;
+    //holds a list of goals
+    private List<string> _listOfGoals;
+    //check if goal is completed
+    private bool _isCompleted;
 
 
     public Goal(string goalName, string goalDescription, int goalAssociatedPoints)
@@ -18,8 +24,32 @@ public abstract class Goal
     }
 
 
+    //sets the properties of the goal
     public virtual void CreateGoal()
     {
-        
+        Console.Write("What is the name of your goal? ");
+        _goalName = Console.ReadLine();
+
+        Console.Write("\nWhat is a short description of it? ");
+        _goalDescription = Console.ReadLine();
+
+        Console.Write("\nWhat is the amount of points associated with this goal? ");
+        string user = Console.ReadLine();
+        _goalAssociatedPoints = int.Parse(user);
+    }
+
+
+    public string GetListOfGoal()
+    {
+        string x;
+        if (_isCompleted == true)
+        {
+            x = "X";
+        }else
+        {
+            x = " ";
+        }
+
+        return ($"[{x}] {_goalName} ({_goalDescription})");
     }
 }
