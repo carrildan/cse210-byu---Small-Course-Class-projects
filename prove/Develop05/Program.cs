8d436201e1585d8533e8a11cb66acb8aca7ea4d1
carrildan <97158875+carrildan@users.noreply.github.com>
2023-03-07 11:34:43 +0100

    created program class, its main menu and submenu

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        //displays main menu
+        Console.WriteLine("Menu Options:\n 1. Create New Goal\n 2. List Goals\n 3. Save Goals\n 4. Load Goals\n 5. Record Event\n 6. Quit");
+        Console.Write("Select a choice from the menu: ");
+        string menuChoice = Console.ReadLine();
+
+        //if user chooses to create a new goal
+        if (menuChoice == "1")
+        {
+            //submenu for 3 different types of goals
+            Console.WriteLine("The types of Goals are:\n 1. Simple Goal\n 2. Eternal Goal\n 3. Checklist Goal");
+            
+            Console.Write("Which type of goal would you like to create? ");
+            string submenuChoice = Console.ReadLine();
+
+
+            if (submenuChoice == "1")
+            {
+                //
+            }
+        }
+
     }
 }
