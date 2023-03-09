67f4ac4245384139d12697f501489d50d78b3320
carrildan <97158875+carrildan@users.noreply.github.com>
2023-03-09 01:38:28 +0100

    added _simpleListOfGoals list and _x (for check goal completed) as attributes | implemented SetListGoal() and DisplayListOfGoal()

--- prove/Develop05/simplegoal.cs
+++ prove/Develop05/simplegoal.cs
@@ -2,9 +2,28 @@ using System;
 
 public class SimpleGoal : Goal
 {
+    private List<string> _simpleListOfGoals;
+    private string _x;
+
+
     public SimpleGoal(string goalName, string goalDescription, string goalAssociatedPoints) : base(goalName, goalDescription, goalAssociatedPoints)
     {
-        
+        _simpleListOfGoals = new List<string>();
+        _x = "X";
+    }
+
+    public override void SetListOfGoal()
+    {
+        _simpleListOfGoals.Add($"The goals are:\n [{_x}] {_goalName} ({_goalDescription})");
+    }
+
+
+    public override void DisplayListOfGoal()
+    {
+        foreach(string item in _simpleListOfGoals)
+        {
+            Console.WriteLine(item);
+        }
     }
 
 }
