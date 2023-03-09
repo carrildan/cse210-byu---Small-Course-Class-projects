9a26d6dd91be21d5da68fcff6a15d529d71ece8e
carrildan <97158875+carrildan@users.noreply.github.com>
2023-03-09 13:50:09 +0100

    added totalPoints property to the constructor | overrided GetDataToSave() to return in the correct format to be saved

--- prove/Develop05/simplegoal.cs
+++ prove/Develop05/simplegoal.cs
@@ -4,10 +4,10 @@ public class SimpleGoal : Goal
 {
     private List<string> _simpleListOfGoals;
     private string _x;
-    private bool _isCompleted;
+    private bool _isCompleted; // MAYBE DELETE IT AND TURN PROTECTED IN BASE CLASS
 
 
-    public SimpleGoal(string goalName, string goalDescription, string goalAssociatedPoints) : base(goalName, goalDescription, goalAssociatedPoints)
+    public SimpleGoal(string goalName, string goalDescription, string goalAssociatedPoints, int totalPoints) : base(goalName, goalDescription, goalAssociatedPoints, totalPoints)
     {
         _simpleListOfGoals = new List<string>();
         _x = " ";
@@ -34,6 +34,13 @@ public class SimpleGoal : Goal
         {
             Console.WriteLine(item);
         }
+    }
+
+
+    //return a string to save object
+    public override string GetDataToSave()
+    {
+       return ($"0\nSimple Goal: {_goalName},{_goalDescription},{_goalAssociatedPoints},{_isCompleted}");
     }
 
 }
