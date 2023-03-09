1d300246b8504dfe0311c39b612d9f72865d4e49
carrildan <97158875+carrildan@users.noreply.github.com>
2023-03-09 02:04:17 +0100

    implemented check mark in SetListOfGoal()

--- prove/Develop05/simplegoal.cs
+++ prove/Develop05/simplegoal.cs
@@ -4,16 +4,26 @@ public class SimpleGoal : Goal
 {
     private List<string> _simpleListOfGoals;
     private string _x;
+    private bool _isCompleted;
 
 
     public SimpleGoal(string goalName, string goalDescription, string goalAssociatedPoints) : base(goalName, goalDescription, goalAssociatedPoints)
     {
         _simpleListOfGoals = new List<string>();
-        _x = "X";
+        _x = " ";
+        _isCompleted = false;
     }
 
     public override void SetListOfGoal()
     {
+        if (_isCompleted == true)
+        {
+            _x = "X";
+        }
+        else
+        {
+            _x = " ";
+        }
         _simpleListOfGoals.Add($"The goals are:\n [{_x}] {_goalName} ({_goalDescription})");
     }
 
