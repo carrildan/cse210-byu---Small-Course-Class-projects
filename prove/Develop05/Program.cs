2f4908d121ecf1eeca83100743186768a80916c6
carrildan <97158875+carrildan@users.noreply.github.com>
2023-03-10 10:07:35 +0100

    added totalPoints variable to use to write once inside a file | added a sum of local variable totalPoints and the one from classes after they are called | the total points now is saved in the file outside the loop

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -6,6 +6,10 @@ class Program
     {
         //instatiate a simple goal to be used when user chooses to create a new goal
         List<Goal> goals = new List<Goal>();
+
+
+        //store the total points of the user to save in the file in menu 3
+        int totalPoints = 1;
         
         
         //displays menu as long the user does not quit the program
@@ -30,37 +34,46 @@ class Program
                 //if user chooses to create a new simple goal
                 if (submenuChoice == "1")
                 {
-                    var newSimpleGoal = new SimpleGoal("unknown","unknown","0", 0);                
+                    var newSimpleGoal = new SimpleGoal("unknown","unknown","0", 100);                
                     newSimpleGoal.CreateGoal();
                     newSimpleGoal.SetListOfGoal();
+                    //sets user´s totalpoints as local variable to be saved in the file
+                    totalPoints += newSimpleGoal.GetTotalPoints();
                     goals.Add(newSimpleGoal);
                 } 
+
                 //if user chooses to create an eternal goal
                 else if (submenuChoice == "2")
                 {
                     var newEternalGoal = new EternalGoal("unknown1","unknown1","1", 0);
                     newEternalGoal.CreateGoal();
                     newEternalGoal.SetListOfGoal();
+                    //sets user´s totalpoints as local variable to be saved in the file
+                    totalPoints += newEternalGoal.GetTotalPoints();
                     goals.Add(newEternalGoal);
-                     
                 }  
+
                 else
                 {
                     var newCheckListGoal = new CheckListGoal("unknown2","unknown2","3", 0);
                     newCheckListGoal.CreateGoal();
                     newCheckListGoal.SetListOfGoal();
+                    //sets user´s totalpoints as local variable to be saved in the file
+                    totalPoints += newCheckListGoal.GetTotalPoints();
                     goals.Add(newCheckListGoal);
                 }
             }
+
             //if user chooses to list the goals
             else if (menuChoice == "2")
             {
-
                 foreach(Goal goal in goals)
                 {
                     goal.DisplayListOfGoal();
                 }       
             }
+            
+            //if user chooses to save as file
             else if (menuChoice == "3")
             {
                 Console.Write("What is the filename for the goal file? ");
@@ -68,6 +81,8 @@ class Program
 
                 using (StreamWriter outputfile = new StreamWriter(fileName))
                 {
+                    //get total points from user and save in the file
+                    outputfile.WriteLine(totalPoints);
                     foreach(Goal goal in goals)
                     {
                         outputfile.WriteLine(goal.GetDataToSave());
