f55925ff7c186b86a60b6ba74a574c3032e631d1
Daniel <car15114@byui.edu>
2023-03-27 11:48:00 +0200

    now constructor(line) content stays in the derived class | updated comments

--- prove/Develop05/simplegoal.cs
+++ prove/Develop05/simplegoal.cs
@@ -2,29 +2,48 @@ using System;
 
 public class SimpleGoal : Goal
 {
-    //private List<string> _simpleListOfGoals;
     private string _goalCompletedCheckMark;
     //private bool _isCompleted; // MAYBE DELETE IT AND TURN PROTECTED IN BASE CLASS
 
 
     public SimpleGoal(string line) : base(line)
     {
+        //specifies the type for this goal
         _goalType = "Simple Goal";
+        
+        //stores the right side of the split(name, description etc)
+        var second = "";
+
+        //stores the left side of the split(type)
+        var firstSplit = line.Split(":");
+        
+        foreach(string word in firstSplit)
+        {
+            _goalType = firstSplit[0];
+            second = firstSplit[1];
+        }
+
+        var last = second.Split(",");
+        for(int i = 0; i < last.Length; i++)
+        {
+            _goalName = last[0];
+            _goalDescription = last[1];
+            _goalAssociatedPoints = last[2];
+        }    
     }
 
     public SimpleGoal(string goalName, string goalDescription, string goalAssociatedPoints, int totalPoints) : base(goalName, goalDescription, goalAssociatedPoints, totalPoints)
     {
-        //_simpleListOfGoals = new List<string>();
         _goalCompletedCheckMark = " ";
         //_isCompleted = true;
         _goalType = "Simple Goal";
     }
 
 
-    //set list in different ways in each class
-    public override string GetListOfGoal()
+    //returns a goal
+    public override string GetGoal()
     {
-        
+        //checks with "X" if goal is completed
         if (_isCompleted == true)
         {
             _goalCompletedCheckMark = "X";
@@ -35,20 +54,18 @@ public class SimpleGoal : Goal
         }
         
         return ($"[{_goalCompletedCheckMark}] {_goalName} ({_goalDescription})");
-
-
     }
 
 
     //displays the itens in the list in different ways in each class
     public override void DisplayListOfGoal()
      {
-        Console.WriteLine(GetListOfGoal());
+        Console.WriteLine(GetGoal());
         //Console.WriteLine($"You have {GetTotalPoints()} points.\n");
     }
 
 
-    //return a string to save object
+    //return a string with data to save object
     public override string Save()
     {
         return ($"{_goalType}:{_goalName},{_goalDescription},{_goalAssociatedPoints},{_isCompleted}");
