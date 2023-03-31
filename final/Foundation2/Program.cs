98823552ca6434e70b21185acc24e2000358b2fd
Daniel <car15114@byui.edu>
2023-03-31 17:03:17 +0200

    created a new customer. testing

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,7 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Product product = new Product("television",150, 100, 5);
-        Console.Write(product.GetProductPrice());
+        Product product = new Product("television",150, 100, 5);     
+
+            
+        Customer customer = new Customer("Daniel");
+        customer.SetAddress("daniel street","valencia","Valencia","valenciana","spain");
+        Console.Write(customer.GetAdress());
+        Console.WriteLine($"\n{customer.CustomerInUsa()}");
+        
+
+
+        
     }
 }
