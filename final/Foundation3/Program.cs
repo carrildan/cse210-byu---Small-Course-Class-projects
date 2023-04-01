b9b74dff3e33dbff34e678d8f59da33fcdc45e41
Daniel <car15114@byui.edu>
2023-04-01 21:53:56 +0200

    testing program. Working so far

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Lectures lecture = new Lectures("AI and the world.","How AI will impact our lives and how to prepare for it.","Lectures","Daniel Carril",200);
+        lecture.SetEventDateAndTime(new System.DateTime(2023, 4, 1), new System.TimeOnly(1,30));
+        lecture.StandardDetailsMessage();
+        lecture.ShortDescriptionMessage();
     }
 }
