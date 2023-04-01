ab88cba7ac248291c182f6a203d6076c299db47f
Daniel <car15114@byui.edu>
2023-04-01 10:43:13 +0200

    created 2 orders with 2 products

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -5,14 +5,27 @@ class Program
     static void Main(string[] args)
     {
         //creates a new customer
-        Customer customer = new Customer("Daniel");
-        customer.SetAddress("daniel street","valencia","Valencia","valenciana","USA");
+        Customer customer = new Customer("Daniel dos Santos Carril");
+        customer.SetAddress("daniel street","sagunto","Valencia","valenciana","Spain");
         
 
         //creates a new order
         Order order = new Order();
-        order.SetProduct(new Product("television",150, 100, 200.50));
+        order.SetProduct(new Product("television",150441, 100, 200.50));
+        order.SetProduct(new Product("computer", 15045, 10, 150.00));
         order.SetCustomer(customer);
-        order.PackingLabel();  
+        order.PackingLabel();
+        order.ShippingLabel();
+        Console.WriteLine(order.TotalCost());  
+
+
+        //Creates a new order
+        Order order2 = new Order();
+        order2.SetProduct(new Product("smartphone samsung", 55748, 50, 100.50));
+        order2.SetProduct(new Product("freezer", 4446698, 2, 950.00));
+        order2.SetCustomer(customer);
+        order2.PackingLabel();
+        order2.ShippingLabel();
+        Console.WriteLine(order2.TotalCost());  
     }
 }
